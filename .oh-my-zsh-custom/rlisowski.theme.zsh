4964da96eb44d09fc247e25eba6ff34d375ce69e
Rafał Lisowski <lisukorin@gmail.com>
2012-08-30 23:38:34 +0200

    zsh

--- .oh-my-zsh-custom/rlisowski.theme.zsh
+++ .oh-my-zsh-custom/rlisowski.theme.zsh
@@ -8,10 +8,21 @@ ZSH_THEME_GIT_PROMPT_AHEAD="%b %{$fg[yellow]%}♦%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_CLEAN="%b"
 ZSH_THEME_GIT_PROMPT_SHA_BEFORE="%b "
 
+ZSH_THEME_SVN_PROMPT_DIRTY="%b %{$fg[yellow]%}✗%{$reset_color%}"
+
 function git_prompt_info() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
   echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(git_prompt_short_sha)$(parse_git_dirty)$(git_prompt_ahead)$ZSH_THEME_GIT_PROMPT_SUFFIX"
 }
 
-RPROMPT='%B$(~/.rvm/bin/rvm-prompt) $(git_prompt_info)%{$reset_color%}'
+function prompt_info() {
+  # only proceed if there is actually a svn repository
+  if [ $(in_svn) ]; then
+    svn_prompt_info
+  else
+    git_prompt_info
+  fi
+}
+
+RPROMPT='%B$(~/.rvm/bin/rvm-prompt) $(prompt_info)%{$reset_color%}'
 
