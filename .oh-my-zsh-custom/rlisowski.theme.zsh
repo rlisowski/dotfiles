ca6827e36d8b3c1e5431f42d5dbcbd1828a31914
Rafał Lisowski <lisukorin@gmail.com>
2012-11-06 20:49:12 +0100

    zsh

--- .oh-my-zsh-custom/rlisowski.theme.zsh
+++ .oh-my-zsh-custom/rlisowski.theme.zsh
@@ -25,14 +25,8 @@ local return_code="%(?..%{$fg[red]%}%? ↵%{$reset_color%})"
 
 local user_host='%{$terminfo[bold]$fg[green]%}%n@%m%{$reset_color%}'
 local current_dir='%{$terminfo[bold]$fg[blue]%} %~%{$reset_color%}'
-local rvm_ruby=''
-if which rvm-prompt &> /dev/null; then
-  rvm_ruby='%{$fg[red]%}‹$(rvm-prompt i v g)›%{$reset_color%}'
-else
-  if which rbenv &> /dev/null; then
-    rvm_ruby='%{$fg[red]%}‹$(rbenv version | sed -e "s/ (set.*$//")›%{$reset_color%}'
-  fi
-fi
+local rvm_ruby='%{$fg[red]%}‹$(rvm-prompt i v p g)›%{$reset_color%}'
+# local rvm_ruby='%{$fg[red]%}‹$(rbenv version | sed -e "s/ (set.*$//")›%{$reset_color%}'
 local rev_info='$(prompt_info)%{$reset_color%}'
 
 PROMPT="╭─ ${rvm_ruby} ${rev_info}${current_dir}
