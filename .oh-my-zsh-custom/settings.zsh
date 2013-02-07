877df72324e0afcc470e84a9b11ca61a2e987ead
Rafał Lisowski <lisukorin@gmail.com>
2013-02-07 16:58:06 +0100

    ruby

--- .oh-my-zsh-custom/settings.zsh
+++ .oh-my-zsh-custom/settings.zsh
@@ -1,8 +1,3 @@
 export TERM=xterm-256color
 export JAVA_HOME=/usr/lib/jvm/jre1.7.0_07
 export MOZILLA_HOME=~/.mozilla
-
-# ruby
-export RUBY_GC_MALLOC_LIMIT=60000000
-export RUBY_FREE_MIN=200000
-export LD_PRELOAD=/usr/lib/libtcmalloc_minimal.so.4
