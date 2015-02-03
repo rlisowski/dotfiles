514882425779a6e537c7394d14406386e1190f54
Rafał Lisowski <lisukorin@gmail.com>
2015-02-03 20:45:03 +0100

    zsh: do not forget ls in history

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -26,13 +26,13 @@ alias afind='ack-grep -il'
 #For example, directories are blue, executable files are green, symlinks are cyan, etc.
 #The -F option appends a symbol after entries to indicate their types.
 #You might not like both options at the same time.
-alias ls=' ls -F --color=tty' #regular ls
-alias l.=' ls -dF .[a-zA-Z0-9]* --color=tty' #only show dotfiles
-alias ll=' ls -lhF --color=tty' #long listing
-alias lsa=' ls -lah'
-alias la=' ls -A'
-alias l=' ls -CF'
-alias sl=' ls' # often screw this up
+alias ls='ls -F --color=tty' #regular ls
+alias l.='ls -dF .[a-zA-Z0-9]* --color=tty' #only show dotfiles
+alias ll='ls -lhF --color=tty' #long listing
+alias lsa='ls -lah'
+alias la='ls -A'
+alias l='ls -CF'
+alias sl='ls' # often screw this up
 
 #Make these commands ask before clobbering a file. Use -F to override.
 #alias rm="rm -i"
